55777cd784e139dce8d279178d1a0886af7df695
Troy Dawson <yortnoswad@gmail.com>
2016-01-01 12:32:17 -0600

    switch basedir to datadir

--- scripts/daily-cleanup.sh
+++ scripts/daily-cleanup.sh
@@ -12,15 +12,15 @@ source /etc/grab-bag.conf
 # CLEANOUT LAST WEEKS HIGH RESOLUTION PICTURES
 ############
 LASTWEEK=`date --date="$HIRESDAYS days ago" +%Y%m%d`
-if [ -d $BASEDIR/$LASTWEEK ] ; then
-	rm -rf $BASEDIR/$LASTWEEK/*/high
+if [ -d $DATADIR/$LASTWEEK ] ; then
+	rm -rf $DATADIR/$LASTWEEK/*/high
 fi
 
 ############
 # CLEAR LOCAL DAY FROM 40 DAYS AGO
 ############
 MONTHSAGO=`date --date="$LOCALKEEPDAYS days ago" +%Y%m%d`
-if [ -d $BASEDIR/$MONTHSAGO ] ; then
-	rm -rf $BASEDIR/$MONTHSAGO
+if [ -d $DATADIR/$MONTHSAGO ] ; then
+	rm -rf $DATADIR/$MONTHSAGO
 fi
 
