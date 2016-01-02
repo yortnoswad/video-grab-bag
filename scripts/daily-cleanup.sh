e3ac2bc95443603648ebd2879ceb1cc053256b78
Limpha Yoklme <limpha@gmail.com>
2016-01-02 09:20:34 -0600

    add debug

--- scripts/daily-cleanup.sh
+++ scripts/daily-cleanup.sh
@@ -12,6 +12,10 @@ source /etc/grab-bag.conf
 # CLEANOUT LAST WEEKS HIGH RESOLUTION PICTURES
 ############
 LASTWEEK=`date --date="$HIRESDAYS days ago" +%Y%m%d`
+if [ "$DEBUG" == "true" ] ; then
+  echo "lastweek: $LASTWEEK  hiresdays: $HIRESDAYS"
+  ls -d $DATADIR/$LASTWEEK
+fi
 if [ -d $DATADIR/$LASTWEEK ] ; then
 	rm -rf $DATADIR/$LASTWEEK/*/high
 fi
@@ -20,6 +24,10 @@ fi
 # CLEAR LOCAL DAY FROM 40 DAYS AGO
 ############
 MONTHSAGO=`date --date="$LOCALKEEPDAYS days ago" +%Y%m%d`
+if [ "$DEBUG" == "true" ] ; then
+  echo "monthsago: $MONTHSAGO  localkeepdays: $LOCALKEEPDAYS"
+  ls -d $DATADIR/$MONTHSAGO
+fi
 if [ -d $DATADIR/$MONTHSAGO ] ; then
 	rm -rf $DATADIR/$MONTHSAGO
 fi
