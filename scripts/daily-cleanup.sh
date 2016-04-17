17c085936ad916a3ff7ad52a494d36f8b0011212
Troy Dawson <yortnoswad@gmail.com>
2016-04-16 19:01:38 -0500

    new directory format

--- scripts/daily-cleanup.sh
+++ scripts/daily-cleanup.sh
@@ -9,9 +9,9 @@ source /etc/grab-bag.conf
 
 
 ############
-# CLEANOUT LAST WEEKS HIGH RESOLUTION PICTURES
+# CLEAR HIGH RESOLUTION PICTURES FROM HIRESDAYS DAYS AGO
 ############
-LASTWEEK=`date --date="$HIRESDAYS days ago" +%Y%m%d`
+LASTWEEK=`date --date="$HIRESDAYS days ago" +%Y/%m/%d`
 if [ "$DEBUG" == "true" ] ; then
   echo "lastweek: $LASTWEEK  hiresdays: $HIRESDAYS"
   ls -d $DATADIR/$LASTWEEK
@@ -21,9 +21,9 @@ if [ -d $DATADIR/$LASTWEEK ] ; then
 fi
 
 ############
-# CLEAR LOCAL DAY FROM 40 DAYS AGO
+# CLEAR LOCAL DAY FROM LOCALKEEPDAYS DAYS AGO
 ############
-MONTHSAGO=`date --date="$LOCALKEEPDAYS days ago" +%Y%m%d`
+MONTHSAGO=`date --date="$LOCALKEEPDAYS days ago" +%Y/%m/%d`
 if [ "$DEBUG" == "true" ] ; then
   echo "monthsago: $MONTHSAGO  localkeepdays: $LOCALKEEPDAYS"
   ls -d $DATADIR/$MONTHSAGO
